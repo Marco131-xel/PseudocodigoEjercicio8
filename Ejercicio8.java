aa0fd39604009b44ba6440bbec74226998d13491
Marco Chiche <marcopoolchichesapon@gmail.com>
2021-06-10 12:48:04 -0600

    Ejercicio8 java

---
+++ Ejercicio8.java
@@ -0,0 +1,22 @@
+public class Ejercicio8{
+    public static void main(String[] args){
+    int contador = 0;
+    System.out.println("");
+    System.out.println("*********Ejercicio8************");
+    System.out.println("");
+    do{
+        int aleatorio = (int)(Math.random()*100+1);
+        if(aleatorio % 5 == 0){
+            System.out.println( aleatorio + "");
+            try {
+       
+                 Thread.sleep(5*20);
+                 }
+                    catch (Exception e) {
+                    System.out.println(e);
+                }
+            contador ++;
+        }
+    }while(contador < 300);
+    }
+}
